eeb1c2845bb837dd791a0a9677f704a20ca5ad9b
ll692 <ll692@cornell.edu>
2021-04-30 22:58:11 -0400

    Last second addition

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -24,26 +24,26 @@ def search():
 		output_message = ''
 		return render_template('search.html', output_message=output_message, data=data )
 	else:
-		if gt == 'Board Games':
-			src = 'data/board-games/data/games_detailed_info.csv'
-			link = 'data/board-games/data/2019_05_02.csv'
-			j = bg.boardgame_jaccard(gn, src, link)
-			c = bg.boardgame_cosine_sim(gn, src)
-			data = bg.combine_cosine_jaccard(c, j)
-		elif gt == 'Mobile Games':
-			j = mg.mgs_jaccard_list(gn)
-			c = mg.mgs_jaccard_list(gn)
-			l = mg.mgs_jacc_cossim(j, c)
-			data = mg.mgs_get_rankings(l)
-		else:
-			appid = sg.steam_name_to_id[gn]
-			l = sg.steam_sim_list(appid)
-			data = sg.steam_get_rankings(l)
-		
+		try:
+			if gt == 'Board Games':
+				src = 'data/board-games/data/games_detailed_info.csv'
+				link = 'data/board-games/data/2019_05_02.csv'
+				j = bg.boardgame_jaccard(gn, src, link)
+				c = bg.boardgame_cosine_sim(gn, src)
+				data = bg.combine_cosine_jaccard(c, j)
+			elif gt == 'Mobile Games':
+				j = mg.mgs_jaccard_list(gn)
+				c = mg.mgs_jaccard_list(gn)
+				l = mg.mgs_jacc_cossim(j, c)
+				data = mg.mgs_get_rankings(l)
+			else:
+				appid = sg.steam_name_to_id[gn]
+				l = sg.steam_sim_list(appid)
+				data = sg.steam_get_rankings(l)
+		except Exception as e:
+			output_message = 'Your query was invalid. Please try searching again.'
+			return render_template('search.html', output_message=output_message)
 		output_message = 'Results of games similar to {' + gn + ', ' + gt + '}' + ':'
-		# except Exception as e:
-		# 	output_message = 'Your query was invalid. Please try searching again.'
-		# 	return render_template('search.html', output_message=output_message)
 		data = data[0:30]
 		#data = [x[0] for x in data]
 		return render_template('search.html', output_message=output_message, gt = gt,  data=data)
