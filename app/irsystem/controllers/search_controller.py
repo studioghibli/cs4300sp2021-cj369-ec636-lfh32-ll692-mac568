f4ef51f204fd0b495661f26aedab610ad66927f8
Linda He <ghibli.hmc.sa.twr@gmail.com>
2021-05-04 15:25:42 -0400

    improve search suggestions

--- app/irsystem/controllers/search_controller.py
+++ app/irsystem/controllers/search_controller.py
@@ -10,36 +10,36 @@ from sim import edit_distance as ed
 
 @irsystem.route('/', methods=['GET', 'POST'])
 def search():
-	if request.method == 'POST':
-		print(request.form)
-		return ed.edit_distance_list(request.form['game_name'], request.form['game_type'])
+    if request.method == 'POST':
+        return ed.edit_distance_list(request.form['game_name'], request.form['game_type'])
 
-	gt = request.args.get('gametype')
-	gn = request.args.get('game')
-	data = []
-	
-	if gt == None or gn == None:
-		output_message = ''
-		return render_template('search.html', output_message=output_message, data=data)
-	else:
-		try:
-			if gt == 'Board Games':
-				src = 'data/board-games/data/games_detailed_info.csv'
-				link = 'data/board-games/data/2019_05_02.csv'
-				j = bg.boardgame_jaccard(gn, src, link)
-				c = bg.boardgame_cosine_sim(gn, src)
-				data = bg.combine_cosine_jaccard(c, j)
-			elif gt == 'Mobile Games':
-				j = mg.mgs_jaccard_list(gn)
-				c = mg.mgs_jaccard_list(gn)
-				l = mg.mgs_jacc_cossim(j, c)
-				data = mg.mgs_get_rankings(l)
-			else:
-				appid = sg.steam_name_to_id[gn]
-				data = sg.steam_get_rankings(sg.steam_sim_list(appid))
-		except Exception as e:
-			output_message = 'Your query was invalid. Please try searching again.'
-			return render_template('search.html', output_message=output_message)
-		output_message = 'Results of games similar to {' + gn + ', ' + gt + '}' + ':'
-		data = data[:30]
-		return render_template('search.html', output_message=output_message, gt = gt, data=data)
+    gt = request.args.get('gametype')
+    gn = request.args.get('game')
+    data = []
+
+    if gt == None or gn == None:
+        output_message = ''
+        return render_template('search.html', output_message=output_message, data=data)
+    else:
+        try:
+            if gt == 'Board Games':
+                src = 'data/board-games/data/games_detailed_info.csv'
+                link = 'data/board-games/data/2019_05_02.csv'
+                j = bg.boardgame_jaccard(gn, src, link)
+                c = bg.boardgame_cosine_sim(gn, src)
+                data = bg.combine_cosine_jaccard(c, j)
+            elif gt == 'Mobile Games':
+                j = mg.mgs_jaccard_list(gn)
+                c = mg.mgs_jaccard_list(gn)
+                l = mg.mgs_jacc_cossim(j, c)
+                data = mg.mgs_get_rankings(l)
+            else:
+                appid = sg.steam_name_to_id[gn]
+                data = sg.steam_get_rankings(sg.steam_sim_list(appid))
+        except Exception as e:
+            output_message = 'Your query was invalid. Please try searching again.'
+            return render_template('search.html', output_message=output_message)
+        output_message = 'Results of games similar to {' + \
+            gn + ', ' + gt + '}' + ':'
+        data = data[:30]
+        return render_template('search.html', output_message=output_message, gt=gt, data=data)
--- sim/edit_distance.py
+++ sim/edit_distance.py
@@ -2,6 +2,7 @@ from sim import board_games as bg
 from sim import mobile_games as mg
 from sim import steam_games as sg
 
+import nltk
 import numpy as np
 import pandas as pd
 
@@ -13,68 +14,6 @@ steam_games = set(pd.Series(sg.steam_df['name'], dtype=str))
 
 all_names = board_games | mobile_games | steam_games
 
-def insertion_cost(message, j):
-    return 1
-
-def deletion_cost(query, i):
-    return 1
-
-def substitution_cost(query, message, i, j):
-    if query[i-1] == message[j-1]:
-        return 0
-    else:
-        return 2
-
-def edit_matrix(query, message):
-    """ calculates the edit matrix
-    
-    Arguments
-    =========
-    query: query string,
-    message: message string,
-    m: length of query + 1,
-    n: length of message + 1,
-    
-    Returns:
-        edit matrix {(i,j): int}
-    """
-    
-    m = len(query) + 1
-    n = len(message) + 1
-    
-    matrix = np.zeros((m, n))
-    for i in range(1, m):
-        matrix[i, 0] = matrix[i-1, 0] + deletion_cost(query, i)
-    
-    for j in range(1, n):
-        matrix[0, j] = matrix[0, j-1] + insertion_cost(message, j)
-    
-    for i in range(1, m):
-        for j in range(1, n):
-            matrix[i, j] = min(
-                matrix[i-1, j] + deletion_cost(query, i),
-                matrix[i, j-1] + insertion_cost(message, j),
-                matrix[i-1, j-1] + substitution_cost(query, message, i, j)
-            )
-    
-    return matrix
-
-def edit_distance(query, message):
-    """ Edit distance calculator
-    
-    Arguments
-    =========
-    query: query string,    
-    message: message string,
-    
-    Returns:
-        edit cost (int)
-    """
-    query = query.lower()
-    message = message.lower()
-    
-    edit_mat = edit_matrix(query, message)
-    return edit_mat[len(query), len(message)]
 
 def edit_distance_list(query, game_type):
     """
@@ -89,10 +28,10 @@ def edit_distance_list(query, game_type):
     elif game_type == 'Video Games':
         all_names = steam_games
     for name in all_names:
-        ranked_names.append((name, edit_distance(query, name)))
+        if query[0].lower() == name[0].lower():
+            ranked_names.append(
+                (name, nltk.edit_distance(query.lower(), name.lower())))
     result = ''
     for tup in sorted(ranked_names, key=lambda x: x[1])[:5]:
         result += tup[0] + ';'
-    return result
-# print(edit_distance_list('hello'))
-# print(edit_distance_list('stanley'))
+    return result[:-1]
