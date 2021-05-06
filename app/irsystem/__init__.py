8affc5d7128662cab17b958ff0414d39f5055db3
Linda He <ghibli.hmc.sa.twr@gmail.com>
2021-05-06 04:58:05 -0400

    remove unused files

--- app/irsystem/__init__.py
+++ app/irsystem/__init__.py
@@ -5,13 +5,3 @@ irsystem = Blueprint('irsystem', __name__, url_prefix='/',static_folder='static'
 
 # Import all controllers
 from .controllers.search_controller import *
-
-from .controllers.popular_controller import *
-
-from .controllers.mobilegames_controller import *
-
-from .controllers.videogames_controller import *
-
-from .controllers.boardgames_controller import *
-
-from .controllers.account_controller import *
